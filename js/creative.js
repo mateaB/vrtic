256200669bfcf699b3383060cbbd6c923c9f69ab
matea bodulusic <matea bodulusic>
2016-10-30 23:11:13 +0100

    s funkcijama

---
+++ js/creative.js
@@ -0,0 +1,7 @@
+function upali(){
+    document.getElementById('myImage').src='http://www.w3schools.com/js/pic_bulbon.gif'
+}
+
+function ugasi() {
+    document.getElementById('myImage').src='http://www.w3schools.com/js/pic_bulboff.gif'
+}
