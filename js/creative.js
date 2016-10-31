4c1fec2cc94e5f19a1c0100128fbbe9192ec90eb
matea bodulusic <matea bodulusic>
2016-10-31 22:04:02 +0100

    Graf

--- js/creative.js
+++ js/creative.js
@@ -1,7 +1,37 @@
-function upali(){
-    document.getElementById('myImage').src='http://www.w3schools.com/js/pic_bulbon.gif'
-}
 
-function ugasi() {
-    document.getElementById('myImage').src='http://www.w3schools.com/js/pic_bulboff.gif'
+
+window.onload = function () {
+	var chart = new CanvasJS.Chart("chartContainer",
+	{
+		title:{
+			text: "Podijela dijece po dobnim skupinama "
+		},
+                animationEnabled: true,
+		legend:{
+			verticalAlign: "center",
+			horizontalAlign: "left",
+			fontSize: 15,
+			fontFamily: "Helvetica"        
+		},
+		theme: "theme4",
+		data: [
+		{        
+			type: "pie",       
+			indexLabelFontFamily: "Garamond",       
+			indexLabelFontSize: 15,
+			indexLabel: "{label} {y}%",
+			startAngle:-10,      
+			showInLegend: true,
+			toolTipContent:"{legendText} {y}",
+			dataPoints: [
+				{  y: 83.24, legendText:"2-3", label: "2-3" },
+				{  y: 8.16, legendText:"3-4", label: "3-4" },
+				{  y: 4.67, legendText:"4-5", label: "4-5" },
+				{  y: 1.67, legendText:"5-6" , label: "5-6"},       
+				{  y: 0.98, legendText:"Others" , label: "Others"}
+			]
+		}
+		]
+	});
+	chart.render();
 }
