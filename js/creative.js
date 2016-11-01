bc2c833fc800193a95f7c319517ab9fa8f3f3279
matea bodulusic <matea bodulusic>
2016-11-01 22:27:47 +0100

    promijena

--- js/creative.js
+++ js/creative.js
@@ -37,6 +37,8 @@ window.onload = function () {
 }
 
 var counter = 0;
+var krug;
+var krizic;
 function promijeni(field) {
 	var current_field = document.getElementById(field);
 	if (current_field.className == "field-changed") {
@@ -45,27 +47,46 @@ function promijeni(field) {
 		if (counter % 2 == 0) {
 			current_field.src = "img/krizic.png";
 			current_field.className = "field-changed";
-			document.getElementById("na-redu").innerHTML = "Na redu: X";
+			krizic = true;
+			document.getElementById("na-redu").innerHTML = "Na redu: O";
 			document.getElementById("error").innerHTML = "";
 		}
 		else{
 			current_field.src = "img/krug.png";
 			current_field.className = "field-changed";
-			document.getElementById("na-redu").innerHTML = "Na redu: Krug";
+			krug = true;
+			document.getElementById("na-redu").innerHTML = "Na redu: X";
 			document.getElementById("error").innerHTML = "";
 		}
 		counter = counter + 1;
+		if(counter === 9){
+			document.getElementById("na-redu").innerHTML = "";
+			var ponovno= prompt("Sva su polja popunjena! Želiš li pokušati ponovno? (da/ne)");
+			if(ponovno === "da"){
+				Resetiraj();
+			}
+		}
 	}
 };
 
 function Resetiraj() {
+	counter = 0;
 	document.getElementById("error").innerHTML = "";
+	document.getElementById("na-redu").innerHTML ="Na redu je: X";
 	var x = document.getElementsByClassName("field-changed");
-	var i;
-	for (i = 0; i < x.length; ) {
-		debugger;
+	for ( var i = 0; i < x.length; ) {
 		x[i].src = "img/kk-blank.png";
 		x[i].className = "field";
-		debugger;
 	};
+}
+
+
+function Rezultat(){
+	if(f1 === krug && f2 === krug && f3 === krug){
+		alert("Pobijedaa!!");
+		var ponovno= prompt("Želiš li pokušati ponovno? (da/ne)");
+			if(ponovno === "da"){
+				Resetiraj();
+			};
+	}
 }
